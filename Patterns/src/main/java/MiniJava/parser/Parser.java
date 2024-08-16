b948f9e799d88c7915748edfb14455fb9750c528
Mohammad Khosravi <mohammad.kh@smartech.ir>
2024-08-16 22:57:57 +0330

    add ParserFacade to apply Facade design pattern

--- Patterns/src/main/java/MiniJava/parser/Parser.java
+++ Patterns/src/main/java/MiniJava/parser/Parser.java
@@ -8,26 +8,23 @@ import java.util.Stack;
 
 import MiniJava.Log.Log;
 import MiniJava.codeGenerator.CodeGeneratorFacade;
-import MiniJava.errorHandler.ErrorHandler;
-import MiniJava.scanner.lexicalAnalyzer;
+import MiniJava.scanner.LexicalAnalyzer;
 import MiniJava.scanner.token.Token;
 
 public class Parser {
-    private ArrayList<Rule> rules;
-    private Stack<Integer> parsStack;
+    private final ArrayList<Rule> rules;
+    private final Stack<Integer> parsStack;
     private ParseTable parseTable;
-    private lexicalAnalyzer lexicalAnalyzer;
-    private final CodeGeneratorFacade codeGeneratorFacade;
 
     public Parser() {
-        parsStack = new Stack<Integer>();
+        parsStack = new Stack<>();
         parsStack.push(0);
         try {
             parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        rules = new ArrayList<Rule>();
+        rules = new ArrayList<>();
         try {
             for (String stringRule : Files.readAllLines(Paths.get("src/main/resources/Rules"))) {
                 rules.add(new Rule(stringRule));
@@ -35,43 +32,9 @@ public class Parser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        codeGeneratorFacade = new CodeGeneratorFacade();
     }
 
-    public void startParse(java.util.Scanner sc) {
-        lexicalAnalyzer = new lexicalAnalyzer(sc);
-        Token lookAhead = lexicalAnalyzer.getNextToken();
-        boolean finish = false;
-        Action currentAction;
-        while (!finish) {
-            try {
-                Log.print(lookAhead.toString() + "\t" + parsStack.peek());
-
-                currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
-
-                Log.print(currentAction.toString());
-
-                switch (currentAction.action) {
-                    case shift:
-                        lookAhead = doShiftAction(currentAction);
-
-                        break;
-                    case reduce:
-                        doReduceAction(currentAction, lookAhead);
-                        break;
-                    case accept:
-                        finish = true;
-                        break;
-                }
-                Log.print("");
-            } catch (Exception ignored) {
-                ignored.printStackTrace();
-            }
-        }
-        if (!ErrorHandler.hasError) codeGeneratorFacade.printMemory();
-    }
-
-    private void doReduceAction(Action currentAction, Token lookAhead) {
+    protected void doReduceAction(Action currentAction, Token lookAhead, CodeGeneratorFacade codeGeneratorFacade) {
         Rule rule = rules.get(currentAction.number);
         for (int i = 0; i < rule.RHS.size(); i++) {
             parsStack.pop();
@@ -89,10 +52,19 @@ public class Parser {
         }
     }
 
-    private Token doShiftAction(Action currentAction) {
+    protected Token doShiftAction(Action currentAction, LexicalAnalyzer lexicalAnalyzer) {
         Token lookAhead;
         parsStack.push(currentAction.number);
         lookAhead = lexicalAnalyzer.getNextToken();
         return lookAhead;
+    }
+
+    protected Integer peekParseStack() {
+        return parsStack.peek();
+    }
+
+    protected Action getCurrentAction(Token lookAhead) {
+        Log.print(lookAhead.toString() + "\t" + peekParseStack());
+        return parseTable.getActionTable(peekParseStack(), lookAhead);
     }
 }
